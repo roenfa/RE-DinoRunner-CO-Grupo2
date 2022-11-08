3c8aa8e2487f4b2d9268ac4ef22f5e1f093290b3
Nestor-MejiaX <117546871+Nestor-MejiaX@users.noreply.github.com>
2022-11-07 19:20:00 -0500

    clase 1 create-dino-components

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,21 @@
+import dino_runner.utils.constants 
+import RUNNING
+class Dinosaur:
+    X_POS = 80
+    Y_POS = 310
+
+    def __init__(self):
+        pass
+
+    def event(self):
+        pass
+
+    def update(self):
+        self.run()
+        pass
+
+    def draw(self):
+        pass
+
+    def run (self):
+        pass
