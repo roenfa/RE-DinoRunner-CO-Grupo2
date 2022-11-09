fc0c16c14124a430ad13be36d3fc5b77612861a1
Rocio Encinas Fajardo <rocio.encinas@usalesiana.edu.bo>
2022-11-09 18:40:59 -0400

    Dino duck

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,15 +1,18 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, DEFAULT_TYPE, JUMPING
+from dino_runner.utils.constants import RUNNING, DEFAULT_TYPE, JUMPING, DUCKING
 from pygame.sprite import Sprite
 
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
     JUMP_VEL = 8.5
 
     def __init__(self):
         self.run_img = {DEFAULT_TYPE: RUNNING}
         self.jump_img = {DEFAULT_TYPE: JUMPING}
+        self.duck_img = {DEFAULT_TYPE: DUCKING}
+
         self.type = DEFAULT_TYPE
         self.image = self.run_img[self.type][0]
         self.dino_rect = self.image.get_rect()
@@ -21,6 +24,7 @@ class Dinosaur(Sprite):
 
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
 
     # Agregar el evento de agacharse para el dino -> pygame.K_DOWN
@@ -28,9 +32,14 @@ class Dinosaur(Sprite):
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_run = False
             self.dino_jump = True
+        elif user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_run = False
+            self.dino_jump = False
+            self.dino_duck = True
         elif not self.dino_jump:
             self.dino_run = True
             self.dino_jump = False
+            self.dino_duck = False
 
     def update(self):
         # self.run()
@@ -38,7 +47,8 @@ class Dinosaur(Sprite):
             self.jump()
         if self.dino_run:
             self.run()
-
+        if self.dino_duck:
+            self.duck()
         
         if self.step_index >= 10:
             self.step_index = 0
@@ -66,6 +76,13 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = self.duck_img[self.type][self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
